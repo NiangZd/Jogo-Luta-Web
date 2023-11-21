04b052ef5533199b1ef951e16f18cd3625fca18d
viniciuscaua <viniciuscaua1504@gmail.com>
2023-11-20 22:53:38 -0300

    Salvando no LocalStorage

--- js/game.js
+++ js/game.js
@@ -15,6 +15,31 @@ let lag = 0;
 
 let currentPhase = 1;
 
+// Função para inicializar o LocalStorage com a pontuação do jogador
+function initLocalStorage() {
+    if (!localStorage.getItem('pontuacao')) {
+        localStorage.setItem('pontuacao', '0');
+    }
+}
+
+// Função para atualizar a pontuação na interface do usuário e salvá-la no localStorage
+function atualizarPontuacao(pontuacao) {
+    const pontosElement = document.getElementById('pontos-atual');
+    pontosElement.textContent = pontuacao; // Atualiza o conteúdo com os pontos do jogador
+
+    // Obtém o jogador atual (você pode substituir isso pelo jogador real)
+    const jogadorAtual = "jogador_teste"; // Substitua pelo jogador real
+
+    // Salva a pontuação associada ao jogador no localStorage
+    salvarPontuacaoLocal(jogadorAtual, pontuacao);
+}
+
+// ... (seu código anterior)
+
+initLocalStorage(); // Chame esta função antes do loop principal do jogo
+
+
+
 function resetEnemies() {
     // Reinicie as propriedades de cada inimigo para a próxima fase
     enemy.setHealth(80);
@@ -52,12 +77,29 @@ function animate() {
         alert("Parabéns! Você venceu o jogo!");
         // Ou reinicie o jogo se necessário
         // location.reload();
+    } else if (player.isDead()) {
+
+        alert("Game Over! Você perdeu!!")
+        // Adicione aqui a lógica para quando o jogador morrer
+        console.log("O jogador morreu!");
+
+        // Dentro da condição de derrota do jogador
+        const pontuacaoAtual = atualizarPontuacao();
+        console.log(`Pontuação atual do jogador: ${pontuacaoAtual}`);
+
+        // Ou use a função de atualização de pontuação
+        atualizarPontuacao(pontuacaoAtual);
+
+        // Pode exibir uma mensagem na tela, reiniciar o jogo, etc.
+        alert("Game Over! O jogador morreu!")
+
     } else {
         // Caso contrário, continue para a próxima fase apenas se a animação de morte estiver concluída
         if (!enemy.isDead() || (enemy.isDead() && enemy.phaseAdvanced)) {
             window.requestAnimationFrame(animate);
         }
     }
+
 }
 
 animate();
